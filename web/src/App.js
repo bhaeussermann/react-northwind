30b601f5bef2e3d8bba1451f1629cd703ffc7243
Bernhard Häussermann <bernhardgh@icloud.com>
2018-12-27 21:27:03 +0200

    Make columns sortable.

--- web/src/App.js
+++ web/src/App.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Spinner from './Common.js';
 
+const SortDirection = Object.freeze({ up: 0, down: 1});
+
 class App extends Component {
+
   state = {
     loading: true,
+    sortedColumn: 'lastName',
+    sortDirection: SortDirection.up,
     employees: []
   };
 
@@ -19,6 +24,36 @@ class App extends Component {
     }, false);
   }
 
+  getColumnClasses(column)
+  {
+    var classes = 'sortable';
+    if (column === this.state.sortedColumn) {
+      classes += ' sort-arrow';
+      classes += this.state.sortDirection === SortDirection.up ? ' up' : ' down';
+    }
+    return classes;
+  }
+
+  toggleSortedColumn(column) {
+    if (column === this.state.sortedColumn) {
+      this.setState({ sortDirection: this.state.sortDirection === SortDirection.up ? SortDirection.down : SortDirection.up });
+    }
+    else {
+      this.setState({ sortedColumn: column, sortDirection: SortDirection.up });
+    }
+  }
+
+  getSortedEmployees() {
+    return this.state.employees.sort((a, b) => {
+      const aValue = a[this.state.sortedColumn], bValue = b[this.state.sortedColumn];
+      if (aValue === bValue)
+        return 0;
+      if (this.state.sortDirection === SortDirection.up)
+        return aValue < bValue ? -1 : 1;
+      return aValue < bValue ? 1 : -1;
+    });
+  }
+  
   render() {
     return (
       <div>
@@ -26,20 +61,24 @@ class App extends Component {
         {this.state.loading 
           ? (<Spinner />) 
           : (
-            <table class="table">
-              <tr>
-                <th class="sortable">Last Name</th>
-                <th class="sortable">FirstName</th>
-                <th class="sortable">Title</th>
-                <th></th>
-              </tr>
-              {this.state.employees.map(e => (
+            <table className="table">
+              <thead>
                 <tr>
-                  <td>{e.lastName}</td>
-                  <td>{e.firstName}</td>
-                  <td>{e.title}</td>
+                  <th className={this.getColumnClasses('lastName')} onClick={() => this.toggleSortedColumn('lastName')}>Last Name</th>
+                  <th className={this.getColumnClasses('firstName')} onClick={() => this.toggleSortedColumn('firstName')}>FirstName</th>
+                  <th className={this.getColumnClasses('title')} onClick={() => this.toggleSortedColumn('title')}>Title</th>
+                  <th></th>
                 </tr>
-              ))}
+              </thead>
+              <tbody>
+                {this.getSortedEmployees().map(e => (
+                  <tr key={e.id}>
+                    <td>{e.lastName}</td>
+                    <td>{e.firstName}</td>
+                    <td>{e.title}</td>
+                  </tr>
+                ))}
+              </tbody>
             </table>
           )
         }
--- web/src/Common.js
+++ web/src/Common.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Common.css';
 
 const Spinner = props => {
-  return (<div class="spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>);
+  return (<div className="spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>);
 }
 
 export default Spinner;
