7e1c361f64e994771d9b7f51012f0cd6783e0bf2
Bernhard Häussermann <bernhardgh@icloud.com>
2018-12-28 21:09:17 +0200

    Add router.

--- web/src/App.js
+++ web/src/App.js
@@ -1,95 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import Common from './Common.js';
-
-const SortDirection = Object.freeze({ up: 0, down: 1});
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Employees from './Employees.js';
 
 class App extends Component {
-  state = {
-    loading: true,
-    loadingError: null,
-    sortedColumn: 'lastName',
-    sortDirection: SortDirection.up,
-    employees: []
-  };
-
-  constructor() {
-    super();
-    const self = this;
-    document.addEventListener('DOMContentLoaded', function() {
-      const controller = new AbortController();
-      Common.timeout(fetch('/northwind/employees', { signal: controller.signal }), 5000)
-        .then(response => response.json())
-        .then(employees => self.setState({ loading: false, employees: employees }))
-        .catch(error => {
-          controller.abort();
-          console.error(error);
-          self.setState({ loading: false, loadingError: error });
-        });
-    }, false);
-  }
-
-  getColumnClasses(column)
-  {
-    var classes = 'sortable';
-    if (column === this.state.sortedColumn) {
-      classes += ' sort-arrow';
-      classes += this.state.sortDirection === SortDirection.up ? ' up' : ' down';
-    }
-    return classes;
-  }
-
-  toggleSortedColumn(column) {
-    if (column === this.state.sortedColumn) {
-      this.setState({ sortDirection: this.state.sortDirection === SortDirection.up ? SortDirection.down : SortDirection.up });
-    }
-    else {
-      this.setState({ sortedColumn: column, sortDirection: SortDirection.up });
-    }
-  }
-
-  getSortedEmployees() {
-    return this.state.employees.sort((a, b) => {
-      const aValue = a[this.state.sortedColumn], bValue = b[this.state.sortedColumn];
-      if (aValue === bValue)
-        return 0;
-      if (this.state.sortDirection === SortDirection.up)
-        return aValue < bValue ? -1 : 1;
-      return aValue < bValue ? 1 : -1;
-    });
-  }
-  
   render() {
     return (
-      <div>
-        <h1>Employees</h1>
-        {this.state.loading 
-          ? (<Common.Spinner />) 
-          : this.state.loadingError
-            ? (<h5>Error retrieving employees: {this.state.loadingError.message}</h5>)
-            : (
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th className={this.getColumnClasses('lastName')} onClick={() => this.toggleSortedColumn('lastName')}>Last Name</th>
-                    <th className={this.getColumnClasses('firstName')} onClick={() => this.toggleSortedColumn('firstName')}>FirstName</th>
-                    <th className={this.getColumnClasses('title')} onClick={() => this.toggleSortedColumn('title')}>Title</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {this.getSortedEmployees().map(e => (
-                    <tr key={e.id}>
-                      <td>{e.lastName}</td>
-                      <td>{e.firstName}</td>
-                      <td>{e.title}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            )
-        }
-      </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path={["/", "/employees"]} exact={true} component={Employees} />
+        <Route path="/" render={props => (<h5>Path not found: {props.location.pathname}</h5>)} />
+      </Switch>
+    </BrowserRouter>
     );
   }
 }
