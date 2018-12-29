adf69729aa77f24b067ba9eee192370860c64a51
Bernhard Häussermann <bernhardgh@icloud.com>
2018-12-29 15:11:09 +0200

    Add Delete employee.

--- web/src/Employees.js
+++ web/src/Employees.js
@@ -6,25 +6,33 @@ const SortDirection = Object.freeze({ up: 0, down: 1});
 
 class Employees extends Component {
   state = {
-    loading: true,
-    loadingError: null,
+    isBusy: true,
+    errorMessage: null,
     sortedColumn: 'lastName',
     sortDirection: SortDirection.up,
-    employees: [],
-    filteredEmployees: []
+    employees: null,
+    filteredEmployees: null
   };
 
   componentDidMount() {
+    this.loadEmployees();
+  };
+
+  async loadEmployees() {
+    this.setState({ isBusy: true, errorMessage: null, employees: null });
     const controller = new AbortController();
-    Common.timeout(fetch('/northwind/employees', { signal: controller.signal }), 5000)
-      .then(response => response.json())
-      .then(employees => this.setState({ loading: false, employees: employees, filteredEmployees: employees }))
-      .catch(error => {
+    try {
+      const response = await Common.timeout(fetch('/northwind/employees', { signal: controller.signal }), 5000);
+      const employees = await response.json();
+      this.setState({ isBusy: false, employees: employees, filteredEmployees: employees });
+    }
+    catch (error) {
         controller.abort();
         console.error(error);
-        this.setState({ loading: false, loadingError: error });
-      });
-  };
+        this.setState({ isBusy: false, errorMessage: 'Error retrieving employees: ' + error.message });
+        throw error;
+    }
+  }
 
   getColumnClasses(column)
   {
@@ -66,38 +74,54 @@ class Employees extends Component {
         || e.title.toLowerCase().indexOf(searchFilter) !== -1);
     this.setState({ filteredEmployees: filteredEmployees });
   }
-    
+
+  async confirmDelete(employee) {
+    if (window.confirm(`Delete ${employee.firstName} ${employee.lastName}?`)) {
+      try {
+        this.setState({ isBusy: true, errorMessage: null });
+        await fetch('/northwind/employees/' + employee.id, { method: 'delete' });
+      }
+      catch (error) {
+        console.error(error);
+        this.setState({ isBusy: false, errorMessage: 'Error deleting employee: ' + error.message });
+        throw error;
+      }
+
+      await this.loadEmployees();
+    }
+  }
+  
   render() {
     return (
       <div>
         <h1>Employees
           <input className="form-control search" placeholder="Search" type="text" onChange={e => this.searchFilterChanged(e)}></input>
         </h1>
-        {this.state.loading 
-          ? (<Common.Spinner />) 
-          : this.state.loadingError
-            ? (<h5>Error retrieving employees: {this.state.loadingError.message}</h5>)
-            : (
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th className={this.getColumnClasses('lastName')} onClick={() => this.toggleSortedColumn('lastName')}>Last Name</th>
-                    <th className={this.getColumnClasses('firstName')} onClick={() => this.toggleSortedColumn('firstName')}>FirstName</th>
-                    <th className={this.getColumnClasses('title')} onClick={() => this.toggleSortedColumn('title')}>Title</th>
-                    <th></th>
+        {this.state.isBusy ? (<Common.Spinner />) : ''}
+        {this.state.errorMessage ? (<h5>{this.state.errorMessage}</h5>) : ''}
+        {!this.state.employees ? ''
+          : (
+            <table className="table">
+              <thead>
+                <tr>
+                  <th className={this.getColumnClasses('lastName')} onClick={() => this.toggleSortedColumn('lastName')}>Last Name</th>
+                  <th className={this.getColumnClasses('firstName')} onClick={() => this.toggleSortedColumn('firstName')}>First Name</th>
+                  <th className={this.getColumnClasses('title')} onClick={() => this.toggleSortedColumn('title')}>Title</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.getSortedEmployees().map(e => (
+                  <tr key={e.id}>
+                    <td>{e.lastName}</td>
+                    <td>{e.firstName}</td>
+                    <td>{e.title}</td>
+                    <td><button className="btn btn-link" onClick={() => this.confirmDelete(e)}>Delete</button></td>
                   </tr>
-                </thead>
-                <tbody>
-                  {this.getSortedEmployees().map(e => (
-                    <tr key={e.id}>
-                      <td>{e.lastName}</td>
-                      <td>{e.firstName}</td>
-                      <td>{e.title}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            )
+                ))}
+              </tbody>
+            </table>
+          )
         }
       </div>
     );
